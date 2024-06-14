31b586b5eb93cd68a0e2ad16e46ab5b5a27fd342
Baliramyadav66 <119006677+Baliramyadav66@users.noreply.github.com>
2024-06-14 19:34:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+   const int MOD = 1000000007;
+    int padovanSequence(int n) {
+        // code here.
+         if (n == 0 || n == 1 || n == 2) return 1;
+
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = dp[2] = 1;
+
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
+        }
+
+        return dp[n];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.padovanSequence(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
